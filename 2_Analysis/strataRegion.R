d6f25099231e801259de67ff9cdce45043c6f6f1
NicolaBarclay <nicola.l.barclay@gmail.com>
2023-06-01 10:05:07 +0100

    tweak

--- 2_Analysis/strataRegion.R
+++ 2_Analysis/strataRegion.R
@@ -1,4 +1,4 @@
-cdm$nb_cancer_covid_cancers_3_time_periods %>% 
+region_distribution <- cdm$nb_cancer_covid_cancers_3_time_periods %>% 
   rename("person_id" = subject_id) %>%
   left_join(cdm$person %>% select(person_id, care_site_id), by = "person_id") %>% 
   left_join(cdm$care_site %>% select(care_site_id, location_id), by = "care_site_id") %>%
@@ -9,3 +9,4 @@ cdm$nb_cancer_covid_cancers_3_time_periods %>%
   tally() %>% 
   arrange(cohort_definition_id) %>%
   collect()
+
